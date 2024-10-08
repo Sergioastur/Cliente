c1153771665c4fc25e1673a67e0e5ac6c24cd6d3
“Sergio <151532958+Sergioastur@users.noreply.github.com>
2024-10-08 11:07:18 +0200

    8-10-24

---
+++ Unidad 2/jsdoc.js
@@ -0,0 +1,10 @@
+/**
+* @summary Calcula la suma de dos números.
+* @description Esta función toma dos números como entrada y devuelve su suma
+* @param {number} x – Primer sumando
+* @param {number} y - Segundo sumando
+* @returns {number} Devuelve la suma de los dos números.
+*/
+function suma(a, b) {
+    return a + b;
+    }
