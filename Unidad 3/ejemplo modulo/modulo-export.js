56541b3045ac65e764282112701e657aedd77552
“Sergio <151532958+Sergioastur@users.noreply.github.com>
2024-10-29 11:12:50 +0100

    29-10-24

---
+++ Unidad 3/ejemplo modulo/modulo-export.js
@@ -0,0 +1,5 @@
+export const hola = () => "Hola";
+export const adios = () => "Adios";
+export default class MiClase {
+    adios = () => "Adios";
+}
---
+++ Unidad 3/ejemplo modulo/modulo.js
@@ -0,0 +1,4 @@
+import MiClaseImportada, {hola} from './modulo-export.js';
+import * as todo from './modulo-export.js';
+let br = "<br>";
+console.log(br,hola);
