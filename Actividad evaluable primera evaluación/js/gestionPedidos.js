dd06c1260cbdba72be9756533f47d3d3bf04b1b1
“Sergio <151532958+Sergioastur@users.noreply.github.com>
2024-11-26 11:13:21 +0100

    26-11-24

--- Actividad evaluable primera evaluación/js/gestionPedidos.js
+++ Actividad evaluable primera evaluación/js/gestionPedidos.js
@@ -1,15 +1,39 @@
 // La aplicación permitirá dar de alta, baja, modificar y consultar piezas de carpintería. 
 function alta () {
+    fechapedido.addEventListener("change", errorChk, false);
     function errorChk() {
-        let hoy = Date.now();
-        let fecha = Date.parse($('#color').val());
-        if (fecha != hoy) {
-            
+        const hoy = new Date();
+        hoy.setHours(0, 0, 0, 0);
+        let fecha = $('#fechapedido').val();
+        const fechaDate = new Date(fecha);
+        
+        console.log($('#procesado').is(":checked"))
+        if (fechaDate.getTime() > hoy.getTime()) {
+            $('#dateErr').removeClass('oculto');
+            $('#submit').attr("disabled", "");
+        } else {
+            $('#dateErr').addClass('oculto');
+            $('#submit').removeAttr("disabled", "");
+
         }
+    }
+    submit.addEventListener("click", guardar, false);
+
+    function guardar() {
+        
+        const datos = {
+            numPedido: $('#numpedido').val(),
+            cliente: $('#cliente').val(),
+            fecha: $('#fechapedido').val(),
+            procesado: $('#procesado').is(":checked"),
+            servido: $('#procesado').is(":checked")
+        };
+
+        localStorage.setItem($('#numpedido').val()+'(pedido)',JSON.stringify(datos));
     }
 
 
-    form.addEventListener("change", errorChk, false);
+    
 
 
 
--- Actividad evaluable primera evaluación/js/gestionPiezas.js
+++ Actividad evaluable primera evaluación/js/gestionPiezas.js
@@ -11,5 +11,60 @@ function altas() {
             $('#noNatural').removeClass('oculto');
         }
     }
+
+    submit.addEventListener("click", guardar, false);
+
+    function guardar() {
+        
+        const datos = {
+            numPedido: $('#numpedido').val(),
+            numPieza: $('#numpieza').val(),
+            largo: $('#largo').val(),
+            ancho: $('#ancho').val(),
+            grosor: $('#grosor').val(),
+            color: $('#color').val(),
+            ambasCaras: $('#ambascaras').is(":checked"),
+            servido: $('input[name="cortada"]').val()
+        };
+
+        localStorage.setItem($('#numpieza').val()+'(pieza)',JSON.stringify(datos));
+    }
     
+    numpieza.addEventListener("change", piezaChk, false);
+
+    function piezaChk() {
+        let encontrado = false;
+        for (let i = 0; i < localStorage.length; i++) {
+            let clave = localStorage.key(i);
+            
+            
+            if (clave == ($('#numpieza').val()+"(pieza)")) {
+                encontrado = true;
+                break;  
+            }
+        }
+
+        if (!Number.isInteger(parseFloat($('#numpieza').val())) || encontrado) {
+            $('#piezaErr').removeClass('oculto');
+            $('#submit').attr("disabled", "");
+        } else {
+            $('#piezaErr').addClass('oculto');
+            $('#submit').removeAttr("disabled", "");
+        }
+    }
+}
+
+
+
+
+function selector() {
+    for (let i = 0; i < localStorage.length; i++) {
+        const clave = localStorage.key(i); 
+        if (clave && clave.endsWith('(pedido)')) {
+            const option = $('<option></option>').attr('value', clave).text(clave);
+            
+            $('#numpedido').append(option);
+        }
+    }
+
 }
