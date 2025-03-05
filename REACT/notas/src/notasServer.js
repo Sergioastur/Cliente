83bb2a2d43e31dd8c46b3869d04392577035f219
“Sergio <151532958+Sergioastur@users.noreply.github.com>
2025-03-05 12:44:17 +0100

    5-03-25

---
+++ REACT/notas/src/notasServer.js
@@ -0,0 +1,43 @@
+const url = "http://localhost:8000/notas.php";
+export const getAllnotas = async () => {
+    const mensajeError = "Error al obtener todas las notas";
+    try {
+        const respuesta = await fetch(url);
+        if (!respuesta.ok) throw new Error(mensajeError);
+        return await respuesta.json();
+    } catch (error) {
+        console.error(mensajeError, error);
+        return [];
+    }
+};
+
+export const getNotaById = async (idNota) => {
+    let mensajeError = `Error al obtener una nota la nota con id ${idNota}`;
+    try {
+        const respuesta = await fetch(`${url}?id=${id}`);
+        if (!respuesta.ok) throw new mensajeError();
+        return await respuesta.json();
+    } catch (error) {
+        console.error(mensajeError, error);
+        return null;
+    }
+};
+
+export const addNota = async (nuevaNota) => {
+    const initObject = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        mode: "cors",
+        body: JSON.stringify(nuevaNota),
+    };
+    const mensajeError = "Error al añadir la nota";
+    try {
+        const respuesta = await fetch(url, initObject);
+        console.log(JSON.stringify(nuevaNota));
+        if (!respuesta.ok) throw new Error(mensajeError);
+        return await respuesta.json();
+    } catch (error) {
+        console.error(mensajeError, error);
+        return null;
+    }
+};
