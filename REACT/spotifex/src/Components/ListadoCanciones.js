816c74a3e0d788bf3c071fe1c38461e7c5d4b21c
Sergioastur <151532958+Sergioastur@users.noreply.github.com>
2025-03-09 15:58:06 +0100

    09-03-25

---
+++ REACT/spotifex/src/Components/ListadoCanciones.js
@@ -0,0 +1,27 @@
+const ListadoCanciones = ({ canciones }) => {
+    return (
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Artista</th>
+            <th>Álbum</th>
+            <th>Duración (s)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {canciones.map((cancion) => (
+            <tr key={cancion.track_id}>
+              <td>{cancion.track_id}</td>
+              <td>{cancion.track_name}</td>
+              <td>{cancion.track_artist}</td>
+              <td>{cancion.track_album_name}</td>
+              <td>{Math.floor(cancion.duration_ms / 1000)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+  export default ListadoCanciones;
