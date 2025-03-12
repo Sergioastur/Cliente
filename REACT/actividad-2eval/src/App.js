c253bdf9b6336aa9d1da53ddac3c1b8ef9c185c6
“Sergio <151532958+Sergioastur@users.noreply.github.com>
2025-03-12 12:52:18 +0100

    Revert "12-03-25"
    This reverts commit 1824903dbc7cfe994326c77a41a0d371d5e8ef8d.

--- REACT/actividad-2eval/src/App.js
+++ REACT/actividad-2eval/src/App.js
@@ -1,12 +1,23 @@
-/* import logo from './logo.svg'; */
+import logo from './logo.svg';
 import './App.css';
-import ControlApp from './components/ControlApp';
 
 function App() {
   return (
     <div className="App">
-      <h1>Gestión diabetes</h1>
-      <ControlApp />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
